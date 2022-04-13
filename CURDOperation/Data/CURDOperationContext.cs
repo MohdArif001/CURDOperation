d0d01c95d253a4c186f1d7c1d3529865459d1302
LENOVO <LENOVO@DESKTOP-PE6MQ5Q>
2022-04-13 12:18:57 +0530

    Add project files.

---
+++ CURDOperation/Data/CURDOperationContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using CURDOperation.Models;
+
+namespace CURDOperation.Data
+{
+    public class CURDOperationContext : DbContext
+    {
+        public CURDOperationContext (DbContextOptions<CURDOperationContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<CURDOperation.Models.Speaker> Speaker { get; set; }
+    }
+}
---
+++ CURDOperation/Migrations/20220215065029_m3.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CURDOperation.Migrations
+{
+    public partial class m3 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Speaker",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    SpeakerName = table.Column<string>(maxLength: 100, nullable: false),
+                    Qualification = table.Column<string>(maxLength: 100, nullable: false),
+                    Experience = table.Column<int>(maxLength: 100, nullable: false),
+                    SpeakingDate = table.Column<DateTime>(nullable: false),
+                    SpeakingTime = table.Column<DateTime>(nullable: false),
+                    Venue = table.Column<string>(maxLength: 255, nullable: false),
+                    ProfilePicture = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Speaker", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Speaker");
+        }
+    }
+}
